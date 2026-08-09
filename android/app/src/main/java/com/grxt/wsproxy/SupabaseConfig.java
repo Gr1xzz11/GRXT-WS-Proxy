5208f5ec2686cb395212e50ddfd55f3999dd2ff4
Gr1xzz11 <grigoryprogramm@gmail.com>
2026-08-09 20:14:56 +0700

    Add Supabase project configuration

---
+++ android/app/src/main/java/com/grxt/wsproxy/SupabaseConfig.java
@@ -0,0 +1,8 @@
+package com.grxt.wsproxy;
+
+final class SupabaseConfig {
+    static final String URL = "https://rxyuxedecsrfezdnnvqb.supabase.co";
+    static final String ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4eXV4ZWRlY3NyZmV6ZG5udnFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyNzE5MTMsImV4cCI6MjEwMTg0NzkxM30.8JQfk8fibsG40GMI5MEdcddk3fdoeCkYajlrspf4OSI";
+
+    private SupabaseConfig() {}
+}
